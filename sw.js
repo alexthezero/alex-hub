528e8353c968f8d174debe3b3dd9454b25f4606d
alexthezero <121217509+alexthezero@users.noreply.github.com>
2026-08-14 15:11:45 -0400

    Increase dashboard text size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "alex-hq-v7";
+const CACHE = "alex-hq-v8";
 const APP = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
