f20de588f625bc1349c6447e48c444f8d46903c0
alexthezero <121217509+alexthezero@users.noreply.github.com>
2026-08-14 13:44:54 -0400

    Polish password gate overflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "alex-hq-v5";
+const CACHE = "alex-hq-v6";
 const APP = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
