c07088b997ff5369fccffa1318fd1bf3648a83f2
alexthezero <121217509+alexthezero@users.noreply.github.com>
2026-08-14 21:28:40 -0400

    Fix mobile navigation overlay and switch to cool blue

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "alex-hq-v19";
-const APP = ["./", "./index.html", "./styles.css?v=18", "./app.js?v=13", "./manifest.webmanifest", "./icon.svg", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "alex-hq-v20";
+const APP = ["./", "./index.html", "./styles.css?v=19", "./app.js?v=13", "./manifest.webmanifest", "./icon.svg", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP)).then(() => self.skipWaiting()));
